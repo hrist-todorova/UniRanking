a760b2d25c001132d223b2e21b0d72161d828190
hrist-todorova <hrist_todorova@abv.bg>
2018-01-21 21:13:59 +0200

    add ID to each wish

--- resources/database/tables/wishes.php
+++ resources/database/tables/wishes.php
@@ -2,9 +2,10 @@
 
 function createTableWishes($connection) {
     $sql = "CREATE TABLE uni_ranking.wishes (
+      ID INT UNSIGNED AUTO_INCREMENT UNIQUE,
       StudentID INT UNSIGNED NOT NULL,
       SpecialityID INT UNSIGNED NOT NULL,
-      Priority INT NOT NULL,
+      Priority INT UNSIGNED NOT NULL,
       PRIMARY KEY (StudentID, SpecialityID, Priority),
       FOREIGN KEY (StudentID) REFERENCES students(ID),
       FOREIGN KEY (SpecialityID) REFERENCES speciality(ID)
